14c801040040e3343f197821517859e40f781d08
Nancy <hanghur@gmail.com>
2024-04-19 13:01:38 +1000

    build: wrap fortran code with f2py, cmake and scikit-build-core

---
+++ pysurf96/surfdisp96_ext.pyi
@@ -0,0 +1,16 @@
+import numpy as np
+
+def surfdisp96(
+    _thk: np.ndarray,
+    _vp: np.ndarray,
+    _vs: np.ndarray,
+    _rho: np.ndarray,
+    nlayers: int,
+    iflsph: int,
+    iwave: int,
+    mode: int,
+    igr: int,
+    kmax: int,
+    t: np.ndarray,
+    result: np.ndarray,
+) -> None: ...
